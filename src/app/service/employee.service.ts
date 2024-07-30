8ab0777684f71536a385e2cf0b8e8d950f4278cc
SAAD-DASTGIR <msaaddastgir@gmail.com>
2024-07-30 14:08:11 +0500

    FEATURE= Added Server Side Pagination

--- src/app/service/employee.service.ts
+++ src/app/service/employee.service.ts
@@ -1,24 +1,28 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class EmployeeService {
-  private apiUrl = 'http://localhost:1337/api/tables';
+  private apiUrl = 'http://localhost:1337/api/tables'; // api from strapi
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {} // injecting HTTPCLIENT to use
 
-  getEmployeeData(): Observable<any> {
-    return this.http.get(this.apiUrl);
+  getEmployeeData(page: number, pageSize: number): Observable<any> { // loading data
+    const params = { // used in endpoint to get paginated data from server
+      'pagination[page]': page.toString(), // converting to string
+      'pagination[pageSize]': pageSize.toString()
+    };
+    return this.http.get(this.apiUrl, { params }); // passing params in endpoint
   }
 
-  addEmployee(employee: any) {
+  addEmployee(employee: any) { // data is used because we get data in array of objects
     return this.http.post(this.apiUrl, { data: employee });
   }
 
-  deleteEmployeeData(id: number) {
+  deleteEmployeeData(id: number) { // as strapi produces default ids therefore we take it as number otherwise locally we would use string
     return this.http.delete(`${this.apiUrl}/${id}`);
   }
 
